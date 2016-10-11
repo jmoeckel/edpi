2f2e42d047ad98a21f7bf55e56f84c75c1a08a0c
Jens Möckel <j.moeckel@udk-berlin.de>
2016-10-11 11:57:49 +0200

    setup.py: Updating version info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="edpi",
     packages=['edpi'],
-    version="0.1.0",
+    version="0.1.1",
     description="An extension of the Dymola-Python interface provided by Dymola itsself.",
     url="https://github.com/jmoeckel/edpi",
     download_url='https://github.com/jmoeckel/edpi/tarball/0.1.0',
