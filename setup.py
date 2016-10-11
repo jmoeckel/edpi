d45425e958ea4817aae1d00074b253383d7c7b07
Jens Möckel <j.moeckel@udk-berlin.de>
2016-10-11 10:51:39 +0200

    Generating setup and license files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages;
+setup(
+    name="edpi",
+    version="0.1.0",
+    description="An extension of the Dymola-Python interface provided by Dymola itsself.",
+    url = "https://github.com/jmoeckel/edpi",
+    author="Jens Möckel",
+    author_email="j.moeckel@udk-berlin.de",
+    packages = [],
+    license = "GPL",
+)
